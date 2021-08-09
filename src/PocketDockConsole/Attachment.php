716dfc9c410ea1c706f2006418a74920c5d6ac91
Blackjack200 <lx826444126@gmail.com>
2021-08-09 19:33:02 +0800

    use composer && change CS

--- src/PocketDockConsole/Attachment.php
+++ src/PocketDockConsole/Attachment.php
@@ -1,18 +1,21 @@
 <?php
+
 namespace PocketDockConsole;
 
+use LoggerAttachment;
 use pocketmine\utils\Terminal;
+use ThreadedLoggerAttachment;
 
-class Attachment extends \ThreadedLoggerAttachment implements \LoggerAttachment {
+class Attachment extends ThreadedLoggerAttachment implements LoggerAttachment {
 
-    public function __construct($thread) {
-        $this->stream = "";
-        $this->thread = $thread;
-    }
+	public function __construct($thread) {
+		$this->stream = "";
+		$this->thread = $thread;
+	}
 
-    public function log($level, $message) {
-        $this->stream.= Terminal::toANSI($message) . "\r\n";
-        $this->thread->stuffToSend = $this->stream;
-    }
+	public function log($level, $message) {
+		$this->stream .= Terminal::toANSI($message) . "\r\n";
+		$this->thread->stuffToSend = $this->stream;
+	}
 
 }
--- src/PocketDockConsole/PDCServer.php
+++ src/PocketDockConsole/PDCServer.php
@@ -1,88 +1,91 @@
 <?php
+
 namespace PocketDockConsole;
 
-use pocketmine\utils\TextFormat;
+use Exception;
+use pocketmine\Thread;
 use pocketmine\utils\Terminal;
+use Wrench\Server;
 
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-class PDCServer extends \pocketmine\Thread {
+class PDCServer extends Thread {
 
-    public $null = NULL;
-    public $buffer = "";
-    public $stuffToSend = "";
-    public $jsonStream = "";
-    public $stuffTitle = "";
-    public $stop = false;
+	public $null = null;
+	public $buffer = "";
+	public $stuffToSend = "";
+	public $jsonStream = "";
+	public $stuffTitle = "";
+	public $stop = false;
 
-    public function __construct($host, $port, $logger, $loader, $password, $html, $backlog, $legacy = false) {
-        $this->host = $host;
-        $this->port = $port;
-        $this->password = $password;
-        $this->logger = $logger;
-        $this->loader = $loader;
-        $this->data = $html;
-        $this->backlog = $backlog;
-        $this->clienttokill = "";
-        $this->sendUpate = false;;
-        $this->legacy = $legacy;
-        $this->start();
-        $this->log("Started SocksServer on " . $this->host . ":" . $this->port);
-    }
+	public function __construct($host, $port, $logger, $loader, $password, $html, $backlog, $legacy = false) {
+		$this->host = $host;
+		$this->port = $port;
+		$this->password = $password;
+		$this->logger = $logger;
+		$this->loader = $loader;
+		$this->data = $html;
+		$this->backlog = $backlog;
+		$this->clienttokill = "";
+		$this->sendUpate = false;
+		$this->legacy = $legacy;
+		$this->start();
+		$this->log("Started SocksServer on " . $this->host . ":" . $this->port);
+	}
 
-    public function getBuffer() {
-        return $this->buffer;
-    }
+	public function log($data) {
+		$this->logger->info("[PDC] " . $data);
+	}
 
-    public function run() {
-        $this->registerClassLoader();
-        set_exception_handler(function ($ex) {
-            //var_dump($ex);
-            $this->logger->debug($ex->getMessage());
-        });
+	public function getBuffer() {
+		return $this->buffer;
+	}
 
-        if (!$this->legacy) {
-            Terminal::init();
-        }
+	public function run() {
+		$this->registerClassLoader();
+		set_exception_handler(function ($ex) {
+			//var_dump($ex);
+			$this->logger->debug($ex->getMessage());
+		});
 
-        $server = new \Wrench\Server('ws://' . $this->host . ':' . $this->port, array("logger" => function ($msg, $pri) {
-        }), $this);
+		if (!$this->legacy) {
+			Terminal::init();
+		}
 
-        $server->registerApplication("app", new PDCApp($this, $this->password));
-        $server->addListener(\Wrench\Server::EVENT_SOCKET_CONNECT, function ($data, $other) {
-            $header = $other->getSocket()->receive();
-            if ($this->isHTTP($header)) {
-                $other->getSocket()->send("HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n" . $this->data);
-                $other->close(200);
-            } else {
-                $other->onData($header);
-            }
-        });
+		$server = new Server('ws://' . $this->host . ':' . $this->port, ["logger" => function ($msg, $pri) {
+		}], $this);
 
-        while($this->stop !== true) {
-            try {
-                $server->run();
-            } catch (\Exception $e) {
+		$server->registerApplication("app", new PDCApp($this, $this->password));
+		$server->addListener(Server::EVENT_SOCKET_CONNECT, function ($data, $other) {
+			$header = $other->getSocket()->receive();
+			if ($this->isHTTP($header)) {
+				$other->getSocket()->send("HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n" . $this->data);
+				$other->close(200);
+			} else {
+				$other->onData($header);
+			}
+		});
 
-            }
-        }
-    }
+		while ($this->stop !== true) {
+			try {
+				$server->run();
+			} catch (Exception $e) {
 
-    public function isHTTP($data) {
-        if (strpos($data, "websocket")) {
-            return false;
-        } else {
-            return true;
-        }
-    }
+			}
+		}
+	}
 
-    public function stop() {
-        $this->stop = true;
-    }
+	public function isHTTP($data) {
+		if (strpos($data, "websocket")) {
+			return false;
+		} else {
+			return true;
+		}
+	}
 
-    public function log($data) {
-        $this->logger->info("[PDC] " . $data);
-    }
+	public function stop() {
+		$this->stop = true;
+	}
 
 }
